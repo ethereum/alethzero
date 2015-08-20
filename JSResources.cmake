86b88c2baf05ff94d615042950338d009547a586
debris <marek.kotewicz@gmail.com>
2015-08-20 14:18:57 +0200

    added JSResources to alethzero

---
+++ JSResources.cmake
@@ -0,0 +1,7 @@
+set(web3    "${ETH_RES_DIR}/libjsqrc/ethereumjs/dist/web3.js")
+set(admin   "${ETH_RES_DIR}/libjsqrc/admin.js")
+set(setup   "${ETH_RES_DIR}/libjsqrc/setup.js")
+
+set(ETH_RESOURCE_NAME "AlethZeroResources")
+set(ETH_RESOURCE_LOCATION "${CMAKE_CURRENT_BINARY_DIR}")
+set(ETH_RESOURCES "web3" "admin" "setup")
