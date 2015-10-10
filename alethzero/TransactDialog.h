a327578a98818259245bf4ffe54239935fd22b7d
arkpar <arkady.paronyan@gmail.com>
2015-10-10 09:37:43 +0200

    gas estimation moved to libethereum

--- alethzero/TransactDialog.h
+++ alethzero/TransactDialog.h
@@ -24,6 +24,7 @@
 #include <QDialog>
 #include <QMap>
 #include <QList>
+#include <QFuture>
 #include <libdevcore/RLP.h>
 #include <libethcore/Common.h>
 #include <libethereum/Transaction.h>
@@ -74,12 +75,17 @@ private slots:
 	void on_send_clicked();
 	void on_debug_clicked();
 	void on_cancel_clicked() { close(); }
+	
+	void updateBounds();
+	void finaliseBounds(dev::eth::ExecutionResult const& _er);
+
+signals:
+	void gasEstimationProgress();
+	void gasEstimationComplete(dev::eth::ExecutionResult const& _er);
 
 private:
 	void rejigData();
 	void updateNonce();
-	void updateBounds();
-	void finaliseBounds();
 
 	dev::Address fromAccount();
 	std::pair<dev::Address, bytes> toAccount();
@@ -92,12 +98,11 @@ private:
 	dev::u256 value() const;
 	dev::u256 gasPrice() const;
 	dev::Address to() const;
-	GasRequirements determineGasRequirements();
+	void determineGasRequirements();
 
 	std::string natspecNotice(dev::Address _to, dev::bytes const& _data);
 	dev::Secret findSecret(dev::u256 _totalReq) const;
 
-	void timerEvent(QTimerEvent*) override;
 
 	AlethFace* aleth() const { return m_aleth; }
 	dev::eth::Client* ethereum() const { return aleth()->ethereum(); }
@@ -114,10 +119,10 @@ private:
 	QString m_dataInfo;
 	qint64 m_startLowerBound = 0;
 	qint64 m_startUpperBound = 0;
-	qint64 m_lowerBound = 0;
-	qint64 m_upperBound = 0;
-	eth::ExecutionResult m_lastGood;
-	int m_gasCalcTimer = 0;
+	std::atomic<qint64> m_lowerBound = { 0 };
+	std::atomic<qint64> m_upperBound = { 0 };
+	QFuture<void> m_estimationFuture;
+	bool m_needsEstimation = false;
 };
 
 }
