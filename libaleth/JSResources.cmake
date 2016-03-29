7a9876462fd519f86d94613f3274519708c73626
chriseth <c@ethdev.com>
2016-03-29 23:52:43 +0200

    Fix required resources.

--- libaleth/JSResources.cmake
+++ libaleth/JSResources.cmake
@@ -1,7 +1,5 @@
 set(web3    "${ETH_RES_DIR}/web3.js/dist/web3.js")
-set(admin   "${ETH_RES_DIR}/webthree-helpers/js/admin.js")
-set(setup   "${ETH_RES_DIR}/webthree-helpers/js/setup.js")
 
 set(ETH_RESOURCE_NAME "AlethResources")
 set(ETH_RESOURCE_LOCATION "${CMAKE_CURRENT_BINARY_DIR}")
-set(ETH_RESOURCES "web3" "admin" "setup")
+set(ETH_RESOURCES "web3")
